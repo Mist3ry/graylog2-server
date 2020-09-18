252cccec6cb9a84f88751999eede0522c9c5a978
Linus Pahl <linus@graylog.com>
2020-09-18 09:24:57 +0200

    Improve wizard validation handling

--- graylog2-web-interface/src/components/common/FormikFormGroup.jsx
+++ graylog2-web-interface/src/components/common/FormikFormGroup.jsx
@@ -1,37 +1,20 @@
 // @flow strict
 import * as React from 'react';
-import { Field } from 'formik';
 
 import FormikInput from './FormikInput';
 
 type Props = {
   label: string,
   name: string,
-  type?: string,
-  help?: string,
-  validate?: (string) => ?string,
-  component?: typeof Field,
 };
 
 /** Displays the FormikInput with a specific layout */
-const FormikFormGroup = ({ component, label, name, type, help, validate, ...rest }: Props) => (
+const FormikFormGroup = ({ label, name, ...rest }: Props) => (
   <FormikInput {...rest}
-               help={help}
                label={label}
                id={name}
-               validate={validate}
                name={name}
-               component={component}
                labelClassName="col-sm-3"
-               wrapperClassName="col-sm-9"
-               type={type} />
+               wrapperClassName="col-sm-9" />
 );
-
-FormikFormGroup.defaultProps = {
-  type: 'text',
-  help: undefined,
-  validate: () => {},
-  component: Field,
-};
-
 export default FormikFormGroup;
